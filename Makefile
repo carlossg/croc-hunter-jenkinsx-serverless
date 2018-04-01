7216d04326b6b394b5713b28b434c4a3b0b416d6
Carlos Sanchez <carlos@apache.org>
2018-04-01 20:43:21 +0200

    croc hunter code

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SHELL := /bin/bash
 GO := GO15VENDOREXPERIMENT=1 go
 NAME := croc-hunter-jenkinsx
 OS := $(shell uname)
-MAIN_GO := hello.go
+MAIN_GO := croc-hunter.go
 ROOT_PACKAGE := $(GIT_PROVIDER)/$(ORG)/$(NAME)
 GO_VERSION := $(shell $(GO) version | sed -e 's/^[^0-9.]*\([0-9.]*\).*/\1/')
 PACKAGE_DIRS := $(shell $(GO) list ./... | grep -v /vendor/)
@@ -18,7 +18,7 @@ check: fmt build test
 build:
 	CGO_ENABLED=$(CGO_ENABLED) $(GO) build -ldflags $(BUILDFLAGS) -o bin/$(NAME) $(MAIN_GO)
 
-test: 
+test:
 	CGO_ENABLED=$(CGO_ENABLED) $(GO) test $(PACKAGE_DIRS) -test.v
 
 full: $(PKGS)
@@ -59,4 +59,3 @@ lint: vendor | $(PKGS) $(GOLINT) # ❷
 	@cd $(BASE) && ret=0 && for pkg in $(PKGS); do \
 	    test -z "$$($(GOLINT) $$pkg | tee /dev/stderr)" || ret=1 ; \
 	done ; exit $$ret
-
---
+++ croc-hunter.go
@@ -0,0 +1,84 @@
+// The infamous "croc-hunter" game as featured at many a demo
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	httpListenAddr := flag.String("port", "8080", "HTTP Listen address.")
+
+	flag.Parse()
+
+	log.Println("Starting server...")
+
+
+	// point / at the handler function
+	http.HandleFunc("/", handler)
+
+	// serve static content from /static
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	log.Println("Server started. Listening on port " + *httpListenAddr)
+	log.Fatal(http.ListenAndServe(":"+*httpListenAddr, nil))
+}
+
+const (
+	html = `
+		<html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+				<title>Croc Hunter</title>
+				<link rel='stylesheet' href='/static/game.css'/>
+				<link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
+				<link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
+			</head>
+			<body>
+				<canvas id="canvasBg" width="800" height="490" ></canvas>
+				<canvas id="canvasEnemy" width="800" height="500" ></canvas>
+				<canvas id="canvasJet" width="800" height="500" ></canvas>
+				<canvas id="canvasHud" width="800" height="500" ></canvas>
+				<script src='/static/game.js'></script>
+				<div class="details">
+				<strong>Hostname: </strong>%s<br>
+				<strong>Release: </strong>%s<br>
+				<strong>Commit: </strong>%s<br>
+				<strong>Powered By: </strong>%s<br>
+				</div>
+			</body>
+		</html>
+		`
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "/healthz" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("could not get hostname: %s", err)
+	}
+
+	release := os.Getenv("WORKFLOW_RELEASE")
+	commit := os.Getenv("GIT_SHA")
+	powered := os.Getenv("POWERED_BY")
+
+	if release == "" {
+		release = "unknown"
+	}
+	if commit == "" {
+		commit = "not present"
+	}
+	if powered == "" {
+		powered = "deis"
+	}
+
+	fmt.Fprintf(w, html, hostname, release, commit, powered)
+}
